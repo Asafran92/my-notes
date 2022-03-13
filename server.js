d97e0835850a3c07f6fef6b8cc256f3fab88b4b5
unknown <avrumiesafranovitz@gmail.com>
2022-03-12 21:38:46 -0500

    added rote for HTML file

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const html = require("./routes/htmlRoutes");
+const api = require("./routes/APIRoutes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use("/", html);
+//app.use("/api", api);
 
 app.listen(PORT, () => {
   console.log(`Listening on port number ${PORT}`);
